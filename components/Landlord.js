b127857f9015033ceffdd17f995d700e52e7052f
54chen <czhttp@gmail.com>
2022-08-09 12:38:14 +1200

    upload picture to aws s3

--- components/Landlord.js
+++ components/Landlord.js
@@ -1,14 +1,26 @@
 import { Container, Row, Col, Form, Group, Label, Button } from 'react-bootstrap';
 import router, { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import Layout from './Layout';
 import { filterData, getFilterValues } from '@/utils/filterData';
 import { baseUrl, fetchApi } from '@/utils/fetchApi';
 import styles from '@/styles/Search.module.css';
 import { useWeb3 } from '../contexts/Web3Context';
+import { FaFileImage,FaFileUpload } from 'react-icons/fa';
+import axios from "axios";
+
+
 
 export default function Landlord() {
+  const BUCKET_URL = "https://awss3stack-bhrpbucket4d926001-1fpwkenr8hw8b.s3.ap-southeast-2.amazonaws.com/";
+  const DEFAULT_IMG = "/images/property.jpg";
+
+  const [file, setFile] = useState()
+  const [uploadingStatus, setUploadingStatus] = useState()
+  const [uploadedFile, setUploadedFile] = useState(DEFAULT_IMG)
+
   const [validated, setValidated] = useState(false);
   const {
     hasWeb3,
@@ -19,6 +31,8 @@ export default function Landlord() {
     isConnected,
     bankBalance,
   } = useWeb3(); 
+  const hiddenFileInput = React.useRef(null);
+
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
@@ -43,7 +57,37 @@ export default function Landlord() {
     }
   }
 
+  const selectFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const uploadFile = async () => {
+    setUploadingStatus("Uploading the file to AWS S3");
+
+    let { data } = await axios.post("/api/s3/uploadFile", {
+      name: file.name,
+      type: file.type,
+    });
+
+    console.log(data);
+
+    const url = data.url;
+    let { data: newData } = await axios.put(url, file, {
+      headers: {
+        "Content-type": file.type,
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    setUploadedFile(BUCKET_URL + file.name);
+    setFile(null);
+  };
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };
+
   return (
+    
     <Row>
     <Col xs={12} md={8}>
     <Form noValidate validated={validated} onSubmit={handleSubmit} className='bg-white p-4 shadow-sm pb-5'>
@@ -103,15 +147,40 @@ export default function Landlord() {
 
       </Row>
       <Row>
+      <Form.Group key="image">
+            <Form.Label htmlFor="image-input">
+            Image of your house  
+            </Form.Label>
+            <Form.Control readOnly required type='input' id='image-input' aria-describedby='image-help' value={uploadedFile}></Form.Control>
+            <Form.Text id="image-help" muted>{DEFAULT_IMG==uploadedFile ? 'Please upload your image of house at first':uploadedFile}</Form.Text>
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+
+      </Row>
+      <Row>
       <Button type="submit">Submit form</Button>
       </Row>
     </Form>
     </Col>
+
     <Col>
-      <div className={styles.agent}>
-        <Image src={`/images/property.jpg`} width={428} height={284} />
-      </div>    
+    <div className='bg-white p-4 mt-4 mt-lg-5 shadow-sm'>
+
+    <Row>
+    <Button onClick={handleClick}><FaFileImage className='me-2' />Select a image of your house</Button>
+      <input ref={hiddenFileInput} style={{display: 'none'}} type="file" onChange={(e) => selectFile(e)} />
+
+        {file && <><p>Image: {file.name} </p><Button onClick={uploadFile}><FaFileUpload className='me-2' />upload!</Button></>
+}
+        {uploadingStatus && <p>{uploadingStatus}</p>}
+    </Row><p></p><Row>
+        {<img src={uploadedFile} width={428} height={284} />}
+        
+      </Row>
+      </div>
+
       </Col>
     </Row>
+
   );
 }
