1a4ad706748dbe54bcaf34c27337de6ebc1d0702
54chen <czhttp@gmail.com>
2022-08-21 10:42:16 +1200

    add random reward to smart contract;

--- hardhat/contracts/WavePortal.sol
+++ hardhat/contracts/WavePortal.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
+    uint256 private seed;
+
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
@@ -13,23 +15,45 @@ contract WavePortal {
         string message; // The message the user sent.
         uint256 timestamp; // The timestamp when the user waved.
     }
+    mapping(address => uint256) public lastWavedAt;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    constructor() payable {
+        console.log("[smart contract]Yo yo, I am a contract and I am smart");
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     mapping(string => Wave[]) private waveMap;
 
     function wave(string memory _message, string memory _to) public {
-        _to = _to;
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 30s"
+        );
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
-        console.log("%s waved w/ message %s to %s", msg.sender, _message, _to);
-        waveMap[_toLower(_to)].push(Wave(msg.sender, _message, block.timestamp));
+        console.log("[smart contract]%s waved w/ message %s to %s", msg.sender, _message, _to);
+        waveMap[_toLower(_to)].push(
+            Wave(msg.sender, _message, block.timestamp)
+        );
         emit NewWave(msg.sender, block.timestamp, _message);
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        uint256 prizeAmount = 0.00001 ether;
+        console.log('[smart contract]balance:',address(this).balance);
+        if (seed <= 50) {
+            prizeAmount = prizeAmount * 2;
+            console.log("[smart contract]%s won! Reward value: %s", msg.sender, prizeAmount);
+        }
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", totalWaves);
+        console.log("[smart contract]We have %d waves!", totalWaves);
         return totalWaves;
     }
 
--- hardhat/scripts/run.js
+++ hardhat/scripts/run.js
@@ -1,42 +1,69 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+  const [owner, randomPerson] = await hre.ethers.getSigners();
 
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
-
-    let waveTxn = await waveContract.wave("A message!",""+owner.address);
-    await waveTxn.wait();
-
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
 
 
-    waveTxn = await waveContract.connect(randomPerson).wave("Another message!",""+waveContract.address);
-    await waveTxn.wait();
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
 
-    let allWaves = await waveContract.getAllWaves();
-    console.log(allWaves);
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log('Count of waves',waveCount.toNumber());
+
+  let waveTxn = await waveContract.wave("A message!", "" + owner.address);
+  await waveTxn.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log('wave count:',waveCount.toNumber());
+
+
+  waveTxn = await waveContract.connect(randomPerson).wave("Another message!", "" + waveContract.address);
+  await waveTxn.wait();
+  contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log('all of the waves cnt:%d', waveCount.toNumber());
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log('count of my waves: %d',allWaves.length);
 };
 
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0); // exit Node process without error
-    } catch (error) {
-        console.log(error);
-        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
-    }
-    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
 };
 
 runMain();
