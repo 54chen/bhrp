8e227d8133f2d683b5e9f9acccea1cc62a9d9bd5
54chen <czhttp@gmail.com>
2022-09-09 11:03:45 +1200

    token smart contracts staking HMT & HGT to get HMT and sometimes get HGT

--- hardhat/contracts/WavePortal.sol
+++ hardhat/contracts/WavePortal.sol
@@ -3,11 +3,133 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 
 contract WavePortal {
     uint256 totalWaves;
     uint256 private seed;
 
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardsToken;
+    address public owner;
+     // Duration of rewards to be paid out (in seconds)
+    uint public duration;
+    // Timestamp of when the rewards finish
+    uint public finishAt;
+    // Minimum of last updated time and reward finish time
+    uint public updatedAt;
+    // Reward to be paid out per second
+    uint public rewardRate;
+    // Sum of (reward rate * dt * 1e18 / total supply)
+    uint public rewardPerTokenStored;
+    // User address => rewardPerTokenStored
+    mapping(address => uint) public userRewardPerTokenPaid;
+    // User address => rewards to be claimed
+    mapping(address => uint) public rewards;
+
+    // Total staked
+    uint public totalSupply;
+    // User address => staked amount
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _stakingToken, address _rewardToken) payable {
+        console.log("[smart contract]Yo yo, I am a contract and I am smart");
+        seed = (block.timestamp + block.difficulty) % 100;
+        owner = msg.sender;
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardToken);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not authorized");
+        _;
+    }
+
+    modifier updateReward(address _account) {
+        rewardPerTokenStored = rewardPerToken();
+        updatedAt = lastTimeRewardApplicable();
+
+        if (_account != address(0)) {
+            rewards[_account] = earned(_account);
+            userRewardPerTokenPaid[_account] = rewardPerTokenStored;
+        }
+
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint) {
+        return _min(finishAt, block.timestamp);
+    }
+
+    function rewardPerToken() public view returns (uint) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+
+        return
+            rewardPerTokenStored +
+            (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18) /
+            totalSupply;
+    }
+
+    function earned(address _account) public view returns (uint) {
+        return
+            ((balanceOf[_account] *
+                (rewardPerToken() - userRewardPerTokenPaid[_account])) / 1e18) +
+            rewards[_account];
+    }
+
+
+    function _min(uint x, uint y) private pure returns (uint) {
+        return x <= y ? x : y;
+    }
+
+    // Reward
+
+    function notifyRewardAmount(uint _amount)
+        external
+        onlyOwner
+        updateReward(address(0))
+    {
+        if (block.timestamp >= finishAt) {
+            rewardRate = _amount / duration;
+        } else {
+            uint remainingRewards = (finishAt - block.timestamp) * rewardRate;
+            rewardRate = (_amount + remainingRewards) / duration;
+        }
+
+        require(rewardRate > 0, "reward rate = 0");
+        require(
+            rewardRate * duration <= rewardsToken.balanceOf(address(this)),
+            "reward amount > balance"
+        );
+
+        finishAt = block.timestamp + duration;
+        updatedAt = block.timestamp;
+    }
+
+    function stake(uint _amount) external updateReward(msg.sender) {
+        require(_amount > 0, "amount = 0");
+        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        balanceOf[msg.sender] += _amount;
+        totalSupply += _amount;
+    }
+
+    function setRewardsDuration(uint _duration) external onlyOwner {
+        require(finishAt < block.timestamp, "reward duration not finished");
+        duration = _duration;
+    }
+
+ 
+    function withdraw(uint _amount) external updateReward(msg.sender) {
+        require(_amount > 0, "amount = 0");
+        balanceOf[msg.sender] -= _amount;
+        totalSupply -= _amount;
+        stakingToken.transfer(msg.sender, _amount);
+    }
+
+    // Wave
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
@@ -16,12 +138,6 @@ contract WavePortal {
         uint256 timestamp; // The timestamp when the user waved.
     }
     mapping(address => uint256) public lastWavedAt;
-
-    constructor() payable {
-        console.log("[smart contract]Yo yo, I am a contract and I am smart");
-        seed = (block.timestamp + block.difficulty) % 100;
-    }
-
     mapping(string => Wave[]) private waveMap;
 
     function wave(string memory _message, string memory _to) public {
--- hardhat/scripts/run.js
+++ hardhat/scripts/run.js
@@ -1,14 +1,40 @@
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
 
+  const HGTContractFactory = await hre.ethers.getContractFactory("HGToken");
+  const HGTContract = await HGTContractFactory.deploy("Housing Governance Token","HGT");
+  await HGTContract.deployed();
+  console.log("HGT deployed to:", HGTContract.address);
+  console.log("HGT deployed by:", owner.address);
+  let hgt = await HGTContract.balanceOf(owner.address);
+  console.log("He has HGT:", hgt);
+
+  const HMTContractFactory = await hre.ethers.getContractFactory("HMToken");
+  const HMTContract = await HMTContractFactory.deploy("Housing Maintenance Token","HGT");
+  await HMTContract.deployed();
+  console.log("HMT deployed to:", HMTContract.address);
+  console.log("HMT deployed by:", owner.address);
+  let hmt = await HMTContract.balanceOf(owner.address);
+  console.log("He has HMT:", hmt);
+
+
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-  const waveContract = await waveContractFactory.deploy({
+  const waveContract = await waveContractFactory.deploy(HMTContract.address, HGTContract.address,{
     value: hre.ethers.utils.parseEther("0.1"),
   });
   await waveContract.deployed();
   console.log("Contract deployed to:", waveContract.address);
   console.log("Contract deployed by:", owner.address);
 
+  // reward settings
+  await waveContract.setRewardsDuration(1);
+  await HMTContract.approve(waveContract.address,10)
+  await waveContract.stake(10);
+
+  await new Promise(r => setTimeout(r, 2000));
+
+  let earned = await waveContract.earned(owner.address);
+  console.log("I earned:", earned);
 
   let contractBalance = await hre.ethers.provider.getBalance(
     waveContract.address
@@ -23,8 +49,7 @@ const main = async () => {
   waveCount = await waveContract.getTotalWaves();
   console.log('Count of waves',waveCount.toNumber());
 
-  let waveTxn = await waveContract.wave("A message!", "" + owner.address);
-  await waveTxn.wait();
+
 
   contractBalance = await hre.ethers.provider.getBalance(
     waveContract.address
