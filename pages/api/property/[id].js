782759b843482ecd073163ac9729c2406b456953
54chen <czhttp@gmail.com>
2022-09-17 19:31:46 +1200

    add nft details json

--- pages/api/property/[id].js
+++ pages/api/property/[id].js
@@ -1,35 +1,19 @@
-import { Container, Row, Col,Carousel } from 'react-bootstrap';
-import { baseUrl, fetchApi } from '@/utils/fetchApi';
-import Layout from '@/components/Layout';
-import Image from 'next/image';
-import About from './about';
-import Features from './features';
-import AgentInfo from './agent';
-import styles from '@/styles/SingleProperty.module.css';
-import {  getNameByCateID } from '@/utils/filterData';
 
 const { PrismaClient } = require('@prisma/client')
 
-export default async function handle({
-  property: { id, room, bath, type, price, desc, wallet, img, ens },
-}){
+export default async function handle(req, res){
 //   {
 //     "name": "Thor's hammer",
 //     "description": "Mjölnir, the legendary hammer of the Norse god of thunder.",
 //     "image": "https://game.example/item-id-8u5h2m.png",
 //     "strength": 20
 // }
-  return res.status(200).json({ name: "HOUSE#"+id, description: desc, image:img, price: price});
-}
-
-export async function getServerSideProps({ params: { id } }) {
+  const { id } = req.query;
   const prisma = new PrismaClient();
   const propertyForRent = await prisma.house.findUnique({
     where: { id: parseInt(id) }
   });
-  return {
-    props: {
-      property: propertyForRent,
-    }
-  };
+  const img = propertyForRent.img!="/images/property.jpg"?propertyForRent.img:"https://bhrp.vercel.app"+propertyForRent.img;
+  return res.status(200).json({ name: "HOUSE#"+propertyForRent.id, description: propertyForRent.desc, image:img, price: propertyForRent.price});
 }
+
